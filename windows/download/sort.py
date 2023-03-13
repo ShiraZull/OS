d930f8579310a90bc084742622cf348722f0f865
ShiraZull <36889707+ShiraZull@users.noreply.github.com>
2023-03-13 17:53:58 +0100

    add a print to detect additional folders

--- windows/download/sort.py
+++ windows/download/sort.py
@@ -1,24 +1,38 @@
 import os
 USERLOGIN = os.getlogin()
 DIRECTORY = 'C:/Users/' + USERLOGIN + '/Downloads'
-SORTFOLDERS = {
+DIR_RULES = {
     "_Folders":{},
-    "Documents":{".pdf", ".doc"},
+    "Documents":{".pdf", ".docx"},
     "Images":{".gif", ".png", ".jpg"},
+    "Videos":{".mp4", ".mkv", ".webm"},
+    "Sound":{".mp3", ".wav", ".webm"},
+    "Executables":{".exe", ".msi"},
+    "Compressed":{".zip", ".rar", ".7z", ".tar.gz"},
     }
 
 entries = os.scandir(DIRECTORY)
+folder_count = 0
+file_count = 0
 for entry in entries:
     if entry.is_dir():
-        print(entry.name)
+        folder_count += 1
+    elif entry.is_file():
+        file_count += 1
 
-print(SORTFOLDERS)
-for thing in SORTFOLDERS:
-    for thinginthing in SORTFOLDERS[thing]:
+print(DIR_RULES)
+for thing in DIR_RULES:
+    for thinginthing in DIR_RULES[thing]:
         print(thinginthing)
 
-for dir in SORTFOLDERS:
-    dirfull = DIRECTORY + '/' + dir
-    if os.path.exists(dirfull) == 0:
-        os.mkdir(dirfull)
-        print("created", dir)
+for dir_name in DIR_RULES:
+    dir_full = DIRECTORY + '/' + dir_name
+    rule_count = len(DIR_RULES[dir_name])
+    if os.path.exists(dir_full) == 0:
+        os.mkdir(dir_full)
+        print("created", dir_name)
+    if rule_count:
+        print(rule_count)
+    elif dir_name == '_Folders':
+        if folder_count > len(DIR_RULES):
+            print(file_count, '-', folder_count, ':', len(DIR_RULES))
