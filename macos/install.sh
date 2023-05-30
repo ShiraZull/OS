5d53b1f39f197ebf6eaafca72cd6c9a8414a6f44
ShiraZull <36889707+ShiraZull@users.noreply.github.com>
2023-05-30 14:55:23 +0200

    Create install.sh

---
+++ macos/install.sh
@@ -0,0 +1,27 @@
+# Code Dev environment
+xcode-select --install
+
+# Install home-brew package manager
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/$USER/.zprofile
+#eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Install packages
+packages=(
+# Utility
+	logi-options-plus
+	idrive
+
+# Documentation
+	google-chrome
+	obs
+
+# Code Environment
+	visual-studio-code
+	git
+	flutter
+	docker
+	gitlab-runner
+)
+
+brew install "${packages[@]}"
